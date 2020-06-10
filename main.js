0d6f0c8c470fccb2c2be7e1e352994e7a6fca0e3
DeepAntala <deep.antala@yahoo.com>
2020-06-09 22:09:39 -0400

    banner

--- main.js
+++ main.js
@@ -7,3 +7,10 @@ hoverout.addEventListener("mouseover", function(){
 hoverout.addEventListener("mouseout", function(){
 	this.classList.add("selected");
 });
+
+$(document).ready(function(){
+			$(window).on("scroll", (function(){
+					$(".main").css("opacity", 1-$(window).scrollTop()/700)
+					$("#banner").css("opacity", 1-$(window).scrollTop()/700)
+				})
+		)})	
